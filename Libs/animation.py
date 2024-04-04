861f60e273c8910f8c07a40db7b70db233ed9d71
b1err <vilmokk@gmail.com>
2024-04-04 23:00:44 +0500

    Final

--- Libs/animation.py
+++ Libs/animation.py
@@ -3,20 +3,18 @@ import time
 import random
 
 def animate(repeat_count):
-    colors = [31, 32, 33, 34, 35, 36]  # Цвета
-    for _ in range(repeat_count):  # Repeat the animation specified number of times
+    colors = [31, 32, 33, 34, 35, 36]
+    for _ in range(repeat_count):
         for char in ['/', '|', '\\', '-']:
-            color_code = random.choice(colors)  # Pick a random color code
-            # ANSI escape sequence for changing text color and moving cursor to the beginning
+            color_code = random.choice(colors)
             sys.stdout.write(f'\r\033[1;{color_code}m{char}\033[0m')
             sys.stdout.flush()
-            time.sleep(0.3)  # Animation delay
+            time.sleep(0.3)
 
 def clear_animation():
-    sys.stdout.write('\r')  # Clear animation from the console
+    sys.stdout.write('\r') # Убираем анимацию
     sys.stdout.flush()
 
-# Define the number of times the animation should repeat
 repeat_count = None  # Кол-во повторений
 
 
