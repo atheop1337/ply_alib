653100252144b6a7a0b589b2ab160c51189c44ca
b1err <vilmokk@gmail.com>
2024-04-05 01:55:17 +0500

    ДАХУЯ

--- Libs/animation.py
+++ Libs/animation.py
@@ -1,13 +1,16 @@
-import sys
 import time
 import random
+from colorama import init, Fore, Style
+import sys
+
+init(autoreset=True)  # Инициализация colorama
 
 def animate(repeat_count):
-    colors = [31, 32, 33, 34, 35, 36]
+    colors = [Fore.RED, Fore.GREEN, Fore.YELLOW, Fore.BLUE, Fore.MAGENTA, Fore.CYAN]
     for _ in range(repeat_count):
         for char in ['/', '|', '\\', '-']:
-            color_code = random.choice(colors)
-            sys.stdout.write(f'\r\033[1;{color_code}m{char}\033[0m')
+            color = random.choice(colors)
+            sys.stdout.write(f'\r{color}{char}{Style.RESET_ALL}')
             sys.stdout.flush()
             time.sleep(0.3)
 
@@ -15,6 +18,7 @@ def clear_animation():
     sys.stdout.write('\r')
     sys.stdout.flush()
 
-repeat_count = None
-
-
+if __name__ == "__main__":
+    repeat_count = 3  # Количество повторений анимации
+    animate(repeat_count)
+    clear_animation()
--- main.py
+++ main.py
@@ -1,33 +1,48 @@
 import requests
 from Libs.random_fact import get_random_fact
 from Libs.animation import animate, clear_animation
-from Libs.headers import setup_headers
+import json
 import time
 import os
+import sys
 
-current_dir = os.path.dirname(os.path.realpath(__file__))
-json_path = os.path.join(current_dir, 'headers.json')
+def setup_headers():
+    exe_path = sys.argv[0]
+    exe_dir = os.path.dirname(os.path.abspath(exe_path))
+    json_path = os.path.join(exe_dir, 'headers.json')
+
+    if not os.path.exists(json_path):
+        raise FileNotFoundError(f'File not found: {json_path}')
+
+    with open(json_path, 'r') as f:
+        headers = json.load(f)
+
+    return headers
+
 url = "https://forum.wayzer.ru/api/users/96"
 headers = setup_headers()
 ids = int(input("Введите ID своего аккаунта: "))
+
 while True:
-    bio = f'Discord: lkqas\n{get_random_fact()}'
+    bio = f'Created by Star boy.\n{get_random_fact()}'
     data = {
         "data": {
             "type": "users",
             "attributes": {
                 "bio": bio,
             },
-            "id": ids # Айди вашего аккаунта
+            "id": ids
         }
     }
-    response = requests.post(url, headers=headers, json=data) # Запрос на смену
-    animate(2)  # Анимация
-    clear_animation()  # Килл
+
+    response = requests.post(url, headers=headers, json=data)
+    animate(2)
+    clear_animation()
+
     if response.status_code == 200 or response.status_code == 201:
-        print(f'\x1b[32mОписание профиля на forum.wayzer.ru успешно изменен на:\n{(bio)}!\x1b[0m\nCode - {response.status_code}')
+        print(f'\x1b[32mОписание профиля на forum.wayzer.ru успешно изменено на:\n{(bio)}!\x1b[0m\nCode - {response.status_code}')
         print(f'\x1b[33mСледующая смена через 60 секунд!')
-        time.sleep(60) # 60 - кол-во секунд через которое меняется рнд факт
+        time.sleep(60)
     else:
         print(f'\x1b[31mОшибка при отправке сообщения: {response.status_code}\x1b[0m')
         print(f'\x1b[31mТекст ошибки: {response.text}\x1b[0m')
