aca9eb7e1201082e53af497dfbadba13d33daac7
b1err <vilmokk@gmail.com>
2024-04-05 00:44:27 +0500

    Added headers in json format, removed as func. The main.py file has been rewritten to accommodate the changes

--- Libs/headers.py
+++ Libs/headers.py
@@ -1,44 +1,6 @@
-def setup_headers(auto):
-    if auto.upper() == "Y":
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0',
-            'Accept': '*/*',
-            'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Referer': 'https://forum.wayzer.ru/u/vilmok',
-            'Content-Type': 'application/json; charset=utf-8',
-            'X-CSRF-Token': 'OIEtpag2MrhzQzj9w3VfxaXZIxrHE4ZTm7Nv6j7M', # Введи свой токен
-            'X-HTTP-Method-Override': 'PATCH',
-            'Origin': 'https://forum.wayzer.ru',
-            'DNT': '1',
-            'Sec-GPC': '1',
-            'Connection': 'keep-alive',
-            'Cookie': 'flarum_remember=UNTKcgOHLYHjC5haEl68pNYPQSkwCrAmnvAYlUHI; flarum_session=BZHHT4yEOgFffbmG54DwsMi790eAyKWq8oWqmvZR', # ВВеди свои куки
-            'Sec-Fetch-Dest': 'empty',
-            'Sec-Fetch-Mode': 'cors',
-            'Sec-Fetch-Site': 'same-origin',
-            'TE': 'trailers'
-        }
-    elif auto.upper() == "N":
-        Cookie = input("Cookie: ")
-        XCFR = input("Token: ")
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0',
-            'Accept': '*/*',
-            'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Referer': 'https://forum.wayzer.ru/u/vilmok',
-            'Content-Type': 'application/json; charset=utf-8',
-            'X-CSRF-Token': XCFR,
-            'X-HTTP-Method-Override': 'PATCH',
-            'Origin': 'https://forum.wayzer.ru',
-            'DNT': '1',
-            'Sec-GPC': '1',
-            'Connection': 'keep-alive',
-            'Cookie': Cookie,
-            'Sec-Fetch-Dest': 'empty',
-            'Sec-Fetch-Mode': 'cors',
-            'Sec-Fetch-Site': 'same-origin',
-            'TE': 'trailers'
-        }
+import json
+def setup_headers():
+    with open('Libs/headers.json', 'r') as file:
+        headers_data = json.load(file)
+        headers = headers_data['headers']
     return headers
--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 import requests
-from Libs.headers import setup_headers
 from Libs.random_fact import get_random_fact
 from Libs.animation import animate, clear_animation
+from Libs.headers import setup_headers
 import time
+import os
 
+current_dir = os.path.dirname(os.path.realpath(__file__))
+json_path = os.path.join(current_dir, 'headers.json')
 url = "https://forum.wayzer.ru/api/users/96"
-auto = input("Auto?:\n(Y/N): ")
-headers = setup_headers(auto)
-
-
+headers = setup_headers()
+ids = int(input("Введите ID своего аккаунта: "))
 while True:
     bio = f'Discord: lkqas\n{get_random_fact()}'
-
     data = {
         "data": {
             "type": "users",
             "attributes": {
                 "bio": bio,
             },
-            "id": "96" # Айди вашего аккаунта
+            "id": ids # Айди вашего аккаунта
         }
     }
     response = requests.post(url, headers=headers, json=data) # Запрос на смену
@@ -26,11 +26,9 @@ while True:
     clear_animation()  # Килл
     if response.status_code == 200 or response.status_code == 201:
         print(f'\x1b[32mОписание профиля на forum.wayzer.ru успешно изменен на:\n{(bio)}!\x1b[0m\nCode - {response.status_code}')
-        print(f'\x1b[33mСледующия смена через 60 секунд!')
+        print(f'\x1b[33mСледующая смена через 60 секунд!')
         time.sleep(60) # 60 - кол-во секунд через которое меняется рнд факт
     else:
         print(f'\x1b[31mОшибка при отправке сообщения: {response.status_code}\x1b[0m')
         print(f'\x1b[31mТекст ошибки: {response.text}\x1b[0m')
         quit()
-
-# Иди нахуй :P
