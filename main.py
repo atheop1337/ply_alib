4027eeb5b6440ac12af529f01253aa73ae997742
EvaCord <ValveSuckdick1@gmail.com>
2024-04-05 22:20:32 +0500

    🖤 | asyncio.sleep

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import requests
-import os
-import json
+import aiohttp
 import asyncio
-import time
+import json
+import os
 import random
 from data.Libs.animation import animate, clear_animation
 
@@ -36,30 +35,26 @@ class ForumBioEditor:
             }
         }
 
-        response = requests.patch(self.link, json=data, headers=headers)
+        async with aiohttp.ClientSession(headers=headers) as session:
+            async with session.patch(self.link, json=data) as response:
+                if response.status != 200:
+                    if self.debug:
+                        print(f"[Bio] // Waiting response...")
+                        await asyncio.sleep(0.01)
+                        print(f"[Bio] // Response code: {response.status}")
+                        await asyncio.sleep(0.01)
+                        print(f"[Bio] // {await response.text()}")
+                    await asyncio.sleep(0.3)
+                    return False
 
-        if response.status_code != 200:  # Если код ответа не равен
-            if self.debug == True:
-                print(f"[Bio] // Waiting response...")
-                time.sleep(0.01)
-                print(f"[Bio] // Response code: {response.status_code}")
-                time.sleep(0.01)
-                print(f"[Bio] // {response.text}")
-            time.sleep(0.3)
-            return False  # Возвращаем False, чтобы основной код мог обработать это условие
-
-        if self.debug == True:
-            print(f"[Bio] // Waiting response...")
-            time.sleep(0.01)
-            print(f"[Bio] // Response code: {response.status_code}")
-            time.sleep(0.01)
-            print("[Bio] // Done!")
-        time.sleep(0.3)
-        return True  # Возвращаем True, если код ответа равен 200
-
-    async def run(self, id):
-        await self.send_request(id)
-
+                if self.debug:
+                    print(f"[Bio] // Waiting response...")
+                    await asyncio.sleep(0.01)
+                    print(f"[Bio] // Response code: {response.status}")
+                    await asyncio.sleep(0.01)
+                    print("[Bio] // Done!")
+                await asyncio.sleep(0.3)
+                return True
 
 class ForumNickEditor:
     def __init__(self, debug):
@@ -91,58 +86,51 @@ class ForumNickEditor:
             }
         }
 
-        response = requests.patch(self.link, json=data, headers=headers)
+        async with aiohttp.ClientSession(headers=headers) as session:
+            async with session.patch(self.link, json=data) as response:
+                if response.status != 200:
+                    if self.debug:
+                        print(f"[Nick] // Waiting response...")
+                        await asyncio.sleep(0.01)
+                        print(f"[Nick] // Response code: {response.status}")
+                        await asyncio.sleep(0.01)
+                        print(f"[Nick] // {await response.text()}")
+                    await asyncio.sleep(0.3)
+                    return False
 
-        if response.status_code != 200:  # Если код ответа не равен 200
-            if self.debug == True:
-                print(f"[Nick] // Waiting response...")
-                time.sleep(0.01)
-                print(f"[Nick] // Response code: {response.status_code}")
-                time.sleep(0.01)
-                print(f"[Nick] // {response.text}")
-            time.sleep(0.3)
-            return False  # Возвращаем False, чтобы основной код мог обработать это условие
+                if self.debug:
+                    print(f"[Nick] // Waiting response...")
+                    await asyncio.sleep(0.01)
+                    print(f"[Nick] // Response code: {response.status}")
+                    await asyncio.sleep(0.01)
+                    print(f"[Nick] // Done!")
+                await asyncio.sleep(0.3)
+                return True
 
-        if self.debug == True:
-            print(f"[Nick] // Waiting response...")
-            time.sleep(0.01)
-            print(f"[Nick] // Response code: {response.status_code}")
-            time.sleep(0.01)
-            print(f"[Nick] // Done!")
-        time.sleep(0.3)
-        return True  # Возвращаем True, если код ответа равен 200
-
-    async def run(self, id, nickname):
-        await self.send_request(id, nickname)
-
-
-def Run(id, delay, nickname, senderbio, sendernick):
+async def Run(id, delay, nickname, senderbio, sendernick):
     loop = 0
     while True:
         animate(delay)
         clear_animation()
-        resultbio = asyncio.run(senderbio.send_request(id))
-        resultnick = asyncio.run(sendernick.send_request(id, nickname))
-        loop = loop + 1
+        resultbio = await senderbio.send_request(id)
+        resultnick = await sendernick.send_request(id, nickname)
+        loop += 1
         print(f"[2501] // Loop: {loop}")
-        time.sleep(0.1)
+        await asyncio.sleep(0.1)
         if not resultbio or not resultnick:
             break
 
-
-def main():
+async def main():
     yn = str(input('[2501] // Debug mode? [y/n]: '))
     user_id = int(input('[2501] // Enter a ID of user: '))
     nickname = str(input('[Nick] // Enter a nickname: '))
     delay = int(input('[2501] // Enter a delay (in seconds): '))
-    debug = (lambda debug: True if debug.lower() == 'y' else False)(yn)
+    debug = True if yn.lower() == 'y' else False
     print(f'[2501] // Debug: {str(debug)}')
-    time.sleep(0.1)
+    await asyncio.sleep(0.1)
     senderbio = ForumBioEditor(debug=debug)
     sendernick = ForumNickEditor(debug=debug)
-    Run(user_id, delay, nickname, senderbio, sendernick)
-
-
+    await Run(user_id, delay, nickname, senderbio, sendernick)
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
