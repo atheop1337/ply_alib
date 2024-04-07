890571489fecde4f1617d4f5335f85eeb759faad
EvaCord <ValveSuckdick1@gmail.com>
2024-04-07 23:13:58 +0500

    🖤 | run updated

--- run.py
+++ run.py
@@ -3,15 +3,8 @@ import os
 current_directory = os.path.dirname(os.path.abspath(__file__))
 
 def run_file(file_path):
-    process = subprocess.Popen(["cmd", "/c", "python", file_path],
-                               creationflags=subprocess.CREATE_NEW_CONSOLE,
-                               stdout=subprocess.PIPE,
-                               stderr=subprocess.PIPE)
-    output, error = process.communicate()
-    print("Output:", output.decode())
-    print("Error:", error.decode())
+    subprocess.Popen(["cmd", "/c", "python", file_path], creationflags=subprocess.CREATE_NEW_CONSOLE)
 
 if __name__ == "__main__":
-    ai_file_path = current_directory + "/main.py"
+    ai_file_path = f"{current_directory}/main.py"
     run_file(ai_file_path)
-
