40f7eb053b77e539ad28d2347329db71089eaa0c
EvaCord <ValveSuckdick1@gmail.com>
2024-04-07 23:02:32 +0500

    🖤 | run updated for v1lmok

--- run.py
+++ run.py
@@ -3,8 +3,15 @@ import os
 current_directory = os.path.dirname(os.path.abspath(__file__))
 
 def run_file(file_path):
-    subprocess.Popen(["cmd", "/c", "python", file_path], creationflags=subprocess.CREATE_NEW_CONSOLE)
+    process = subprocess.Popen(["cmd", "/c", "python", file_path],
+                               creationflags=subprocess.CREATE_NEW_CONSOLE,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+    output, error = process.communicate()
+    print("Output:", output.decode())
+    print("Error:", error.decode())
 
 if __name__ == "__main__":
     ai_file_path = current_directory + "/main.py"
     run_file(ai_file_path)
+
