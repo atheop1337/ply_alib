54615547e55a30127d82de553687e826bbade87a
b1err <vilmokk@gmail.com>
2024-04-08 00:03:31 +0500

    🤡 | Мужло, помолчи пж

--- data/libraries/generate_quote_jsontable.py
+++ data/libraries/generate_quote_jsontable.py
@@ -17,6 +17,7 @@ def generate_citata():
                 citata_text = citata_tag.text.strip()
                 if len(citata_text) <= 60:
                     quotes.append(citata_text)
+                    print(citata_text)
         except Exception as e:
             print("Error fetching data:", e)
     return quotes
