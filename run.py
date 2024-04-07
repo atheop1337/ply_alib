6b1bb5df97faddd10978654c2e415f5092c8d3ca
EvaCord <ValveSuckdick1@gmail.com>
2024-04-07 23:00:53 +0500

    🖤 | run updated for v1lmok

--- run.py
+++ run.py
@@ -6,5 +6,5 @@ def run_file(file_path):
     subprocess.Popen(["cmd", "/c", "python", file_path], creationflags=subprocess.CREATE_NEW_CONSOLE)
 
 if __name__ == "__main__":
-    ai_file_path = "main.py"
+    ai_file_path = current_directory + "/main.py"
     run_file(ai_file_path)
