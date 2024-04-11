8260ca521d4a55144f3b3f2122af6d9e22fd272c
b1err <vilmokk@gmail.com>
2024-04-11 09:54:13 +0500

    ⚛ | RPC-fix

--- data/libraries/Inters.py
+++ data/libraries/Inters.py
@@ -9,7 +9,7 @@ def rpc_connect():
     rpc = discordrpc.RPC(app_id=1227178799964356650, output=True)
     button = discordrpc.Button(button_one_label="Какой-то мужик", button_one_url="https://github.com/v1lmok",button_two_label="Злодей британец",button_two_url="https://github.com/PivoSteve")
     rpc.set_activity(
-        state=f'{data}', #2
+        state=f'Actual version {data}', #2
         details='Powered by 2501', #1
         buttons=button,
         large_image='main',
