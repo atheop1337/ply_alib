a08ea47061abb84efd64fa83b86c567b174622de
b1err <vilmokk@gmail.com>
2024-04-11 14:12:55 +0500

    ⚛ | RPC random image

--- data/libraries/Inters.py
+++ data/libraries/Inters.py
@@ -1,4 +1,5 @@
 import discordrpc
+import random
 from discordrpc.utils import timestamp
 from twentyfivezeroone import Connection
 get_version = Connection
@@ -7,12 +8,13 @@ data = Connection().get_version()
 
 def rpc_connect():
     rpc = discordrpc.RPC(app_id=1227178799964356650, output=True)
+    image = ['main', 'poebat', '2501'][random.randint(0, 2)]
     button = discordrpc.Button(button_one_label="Какой-то мужик", button_one_url="https://github.com/v1lmok",button_two_label="Злодей британец",button_two_url="https://github.com/PivoSteve")
     rpc.set_activity(
         state=f'Actual version {data}', #2
         details='Powered by 2501', #1
         buttons=button,
-        large_image='main',
+        large_image=image,
         large_text='Мейби бейби такая зайка',
         ts_start=timestamp,
         ts_end=86400000000,
