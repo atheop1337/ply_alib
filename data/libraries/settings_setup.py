8c249f0fde20f018f9d486e82a2ad59fbd4b5e30
Pivo <horriblebuba@gmail.com>
2024-04-15 21:55:24 +0500

    🖤 | settings complete

--- data/libraries/settings_setup.py
+++ data/libraries/settings_setup.py
@@ -1,74 +1,45 @@
+import configparser
 import inquirer
 import time
-import subprocess
 import os
 import signal
 import sys
-from data.libraries.twentyfivezeroone import WindowTitle
 from colorama import Fore, Style, init
+from twentyfivezeroone import const, WindowTitle
 current_directory = os.path.dirname(os.path.abspath(__file__))
 init(autoreset=True)
 
 def signal_handler(sig, frame):
-    print(f"\n{Fore.RESET}{Style.DIM}[2501] {Fore.YELLOW}// Shutdown signal received...")
+    print(f"{Fore.RESET}{Style.DIM}[2501] {Fore.YELLOW}// Shutdown signal received...")
     print(f"{Fore.RESET}{Style.DIM}[2501] {Fore.YELLOW}// Cleaning up...")
     time.sleep(0.5)
     print(f"{Fore.RESET}{Style.DIM}[2501] {Fore.YELLOW}// Thank you for being with us!")
     time.sleep(2)
     sys.exit(0)
 
-def run_file(file_path, show_console=True):
-    if show_console:
-        subprocess.Popen(["cmd", "/c", "python", file_path], creationflags=subprocess.CREATE_NEW_CONSOLE)
-    else:
-        subprocess.Popen(["cmd", "/c", "python", file_path])
-    return True
-
-def all_in_one():
-    print(f'\n{Fore.RED}THIS FUNCTION IS UNDER CONSTRUCT!')
-    #run_file(f'{current_directory}/data/run_all.py', False)
-    time.sleep(5)
-    main()
-
-def setup():
-    run_file(f'{current_directory}/data/run_setup.py', True)
-    time.sleep(5)
-    main()
-
-def main_script():
-    run_file(f'{current_directory}/data/run_main.py', False)
-    time.sleep(5)
-    main()
-
-def server_monitoring():
-    run_file(f'{current_directory}/data/run_serverMonitor.py', True)
-    time.sleep(5)
-    main()
-
-def plug():
-    print(f'\n{Fore.RED}THIS FUNCTION IS UNDER CONSTRUCT!')
-    time.sleep(5)
-    main()
-
-def list_input(choice):
-    choice_options = {
-        '[!] all in one': all_in_one,
-        '[!] setup hub': setup,
-        '[•] main script': main_script,
-        '[•] server monitoring': server_monitoring,
-        '[•] someother shit': plug,
+def create_settings_ini(answers):
+    config = configparser.ConfigParser()
+    config["serverMonitor"] = {
+        "; Delay between updates in seconds": "Default 60",
+        "delay": answers["server_delay"]
     }
-    chosen_function = choice_options.get(choice)
-    if chosen_function is not None:
-        chosen_function()
+    config["quotesGenerator"] = {
+        "; Amount of quotes to be generated": "Default 50",
+        "amount": answers["quotes_amount"]
+    }
+    with open(const().directory + "/settings.ini", "w") as configfile:
+        config.write(configfile)
+    print(f"\n{Fore.RESET}{Style.DIM}[2501] // {Fore.YELLOW}Settings file created successfully in {const().directory}!")
+    time.sleep(2)
+    signal_handler(None, None)
 
 
 def main():
-    WindowTitle().set("ply_Alib   //   Hub")
-    os.system('cls' if os.name == 'nt' else 'clear')
+    WindowTitle().set("ply_Alib   //   Settings")
+    signal.signal(signal.SIGINT, signal_handler)
     print(f"""{Fore.LIGHTWHITE_EX}{Style.DIM}
 ┌──────────────────┬────────────────────────────────────────────────────────────┐
-│     {Fore.RESET}ply_Alib       Hub                                                        {Fore.LIGHTWHITE_EX}│
+│     {Fore.RESET}ply_Alib       Settings                                                   {Fore.LIGHTWHITE_EX}│
 ├──────────────────┴────────────────────────────────────────────────────────────┤
 │                                                                               │
 │{Fore.YELLOW}         ██▓███   ██▓   ▓██   ██▓       ▄▄▄       ██▓     ██▓ ▄▄▄▄             {Fore.LIGHTWHITE_EX}│
@@ -83,25 +54,22 @@ def main():
 │{Fore.YELLOW}                      ░ ░                                       ░              {Fore.LIGHTWHITE_EX}│
 │                                                                               │
 │  {Fore.RESET}[•]   {Fore.GREEN}Welcome to the ply_Alib script!                                        {Fore.LIGHTWHITE_EX}│
-│  {Fore.RESET}[!]   {Fore.GREEN}Choose your mode to open ply_Alib!                                     {Fore.LIGHTWHITE_EX}│
+│  {Fore.RESET}[!]   {Fore.GREEN}Choose your mode to setup ply_Alib!                                    {Fore.LIGHTWHITE_EX}│
 │  {Fore.RESET}[@]   {Fore.RED}Closing the terminal window is NOT SAFE please use {Fore.RESET}CTRL+C{Fore.RED}.             {Fore.LIGHTWHITE_EX}│
 └───────────────────────────────────────────────────────────────────────────────┘
 """)
     time.sleep(0.2)
-    signal.signal(signal.SIGINT, signal_handler)
     try:
-        choice = inquirer.list_input(f"{Fore.RESET}{Style.DIM}[2501] // {Fore.GREEN}Enter your choice{Fore.RESET}", choices=[
-            '[!] all in one',
-            '[!] setup hub',
-            '[•] main script',
-            '[•] server monitoring',
-            '[•] someother shit',
-        ])
+        questions = [
+            inquirer.Text("server_delay", message=f"{Fore.RESET}{Style.DIM}[2501] // {Fore.GREEN}Enter delay for \"serverMonitor\" section (in seconds){Fore.RESET}", default="60"),
+            inquirer.Text("quotes_amount", message=f"{Fore.RESET}{Style.DIM}[2501] // {Fore.GREEN}Enter amount for \"quotesGenerator\" section{Fore.RESET}", default="50"),
+        ]
     except KeyboardInterrupt:
         signal_handler(None, None)
 
-    list_input(choice)
+    answers = inquirer.prompt(questions)
+    create_settings_ini(answers)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- data/run_setup.py
+++ data/run_setup.py
@@ -4,7 +4,7 @@ import subprocess
 import os
 import signal
 import sys
-from data.libraries.twentyfivezeroone import WindowTitle
+from libraries.twentyfivezeroone import WindowTitle
 from colorama import Fore, Style, init
 current_directory = os.path.dirname(os.path.abspath(__file__))
 init(autoreset=True)
@@ -24,51 +24,39 @@ def run_file(file_path, show_console=True):
         subprocess.Popen(["cmd", "/c", "python", file_path])
     return True
 
-def all_in_one():
+def quote_req():
+    run_file(f'{current_directory}/libraries/generate_quote_jsontable.py', True)
+    time.sleep(5)
+    main()
+
+def settings_req():
     print(f'\n{Fore.RED}THIS FUNCTION IS UNDER CONSTRUCT!')
-    #run_file(f'{current_directory}/data/run_all.py', False)
+    run_file(f'{current_directory}/libraries/settings_setup.py', True)
     time.sleep(5)
     main()
 
-def setup():
-    run_file(f'{current_directory}/data/run_setup.py', True)
-    time.sleep(5)
-    main()
-
-def main_script():
-    run_file(f'{current_directory}/data/run_main.py', False)
-    time.sleep(5)
-    main()
-
-def server_monitoring():
-    run_file(f'{current_directory}/data/run_serverMonitor.py', True)
-    time.sleep(5)
-    main()
-
-def plug():
-    print(f'\n{Fore.RED}THIS FUNCTION IS UNDER CONSTRUCT!')
+def token_req():
+    run_file(f'{current_directory}/GetToken.py', False)
     time.sleep(5)
     main()
 
 def list_input(choice):
     choice_options = {
-        '[!] all in one': all_in_one,
-        '[!] setup hub': setup,
-        '[•] main script': main_script,
-        '[•] server monitoring': server_monitoring,
-        '[•] someother shit': plug,
+        '[!] token&cookies': token_req,
+        '[!] settings': settings_req,
+        '[!] quotes': quote_req,
     }
     chosen_function = choice_options.get(choice)
     if chosen_function is not None:
         chosen_function()
 
-
 def main():
-    WindowTitle().set("ply_Alib   //   Hub")
+    WindowTitle().set("ply_Alib   //   Setup")
+    signal.signal(signal.SIGINT, signal_handler)
     os.system('cls' if os.name == 'nt' else 'clear')
     print(f"""{Fore.LIGHTWHITE_EX}{Style.DIM}
 ┌──────────────────┬────────────────────────────────────────────────────────────┐
-│     {Fore.RESET}ply_Alib       Hub                                                        {Fore.LIGHTWHITE_EX}│
+│     {Fore.RESET}ply_Alib       Setup                                                      {Fore.LIGHTWHITE_EX}│
 ├──────────────────┴────────────────────────────────────────────────────────────┤
 │                                                                               │
 │{Fore.YELLOW}         ██▓███   ██▓   ▓██   ██▓       ▄▄▄       ██▓     ██▓ ▄▄▄▄             {Fore.LIGHTWHITE_EX}│
@@ -83,25 +71,21 @@ def main():
 │{Fore.YELLOW}                      ░ ░                                       ░              {Fore.LIGHTWHITE_EX}│
 │                                                                               │
 │  {Fore.RESET}[•]   {Fore.GREEN}Welcome to the ply_Alib script!                                        {Fore.LIGHTWHITE_EX}│
-│  {Fore.RESET}[!]   {Fore.GREEN}Choose your mode to open ply_Alib!                                     {Fore.LIGHTWHITE_EX}│
+│  {Fore.RESET}[!]   {Fore.GREEN}Choose your mode to setup ply_Alib!                                    {Fore.LIGHTWHITE_EX}│
 │  {Fore.RESET}[@]   {Fore.RED}Closing the terminal window is NOT SAFE please use {Fore.RESET}CTRL+C{Fore.RED}.             {Fore.LIGHTWHITE_EX}│
 └───────────────────────────────────────────────────────────────────────────────┘
 """)
     time.sleep(0.2)
-    signal.signal(signal.SIGINT, signal_handler)
     try:
         choice = inquirer.list_input(f"{Fore.RESET}{Style.DIM}[2501] // {Fore.GREEN}Enter your choice{Fore.RESET}", choices=[
-            '[!] all in one',
-            '[!] setup hub',
-            '[•] main script',
-            '[•] server monitoring',
-            '[•] someother shit',
+            '[!] token&cookies',
+            '[!] settings',
+            '[!] quotes',
         ])
     except KeyboardInterrupt:
         signal_handler(None, None)
 
     list_input(choice)
-
 
 if __name__ == '__main__':
     main()
