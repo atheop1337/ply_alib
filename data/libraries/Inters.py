07ff00fb094b1fe8f5dc1e040498a3ef7fc88434
b1err <vilmokk@gmail.com>
2024-04-16 21:26:54 +0500

    ⚛ | Adding avatarUrl and displayName in small image/small text

--- data/libraries/Inters.py
+++ data/libraries/Inters.py
@@ -3,8 +3,9 @@ import psutil
 import os
 import json
 import time
+import asyncio
 from discordrpc.utils import timestamp
-from twentyfivezeroone import Connection, RandomStr, const
+from twentyfivezeroone import Connection, RandomStr, Get_AV, const
 get_version = Connection
 data = Connection().get_version()
 
@@ -32,20 +33,26 @@ def readjsonloop(rpc):
 
 def rpc_connect():
     rpc = discordrpc.RPC(app_id=1227178799964356650, output=True)
-    button = discordrpc.Button(button_one_label="v1lmok", button_one_url="https://github.com/v1lmok",button_two_label="Pivo",button_two_url="https://github.com/PivoSteve")
+    av = Get_AV()
+    avatar, name = asyncio.run(av.get_data(av.amount))
+    button = discordrpc.Button(
+        button_one_label="v1lmok",
+        button_one_url="https://github.com/v1lmok",
+        button_two_label="Pivo",
+        button_two_url="https://github.com/PivoSteve"
+    )
     rpc.set_activity(
-        state=f'Actual version: {data}', #2
-        details='Powered by 2501', #1
+        state=f'Actual version: {data}',
+        details='Powered by 2501',
         buttons=button,
         large_image='2501m',
-        large_text='born in the sea of information',
-        small_image='https://cdn.discordapp.com/attachments/678147298009546752/1229736508450275398/tumblr_7eba1b0e282033eac7e9702c4f3bbb2a_e58f7d7d_500.gif',
-        small_text='Весть о наступлении Царства Божия и спасении рода человеческого',
+        large_text='Born in the sea of information',
+        small_image=avatar,
+        small_text=name,
         ts_start=timestamp,
         ts_end=86400000000,
-        )
+    )
     readjsonloop(rpc)
     rpc.run()
-
 
 rpc_connect()
